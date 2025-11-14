b4c67c4d3df00461581305b3e74b773cc868d0b4
rafamercado <rafaelmercado@csu.fullerton.edu>
2025-11-14 06:44:06 -0800

    Implmenting the Flask  mail for contact us page.

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_mail import Mail
 
 db = SQLAlchemy()
+mail = Mail()
+
 DB_NAME = "cars.db"
 
 def create_app():
@@ -10,7 +13,15 @@ def create_app():
     app.config['SECRET_KEY'] = 'your_secret_key'
     app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DB_NAME}"
 
+    # Email notification configuration
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    app.config['MAIL_PORT'] = 587
+    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_USERNAME'] = 'Rafaelmercadoespinoza@gmail.com'
+    app.config['MAIL_PASSWORD'] = '554420938'  # replace with real Gmail App Password
+
     db.init_app(app)
+    mail.init_app(app)
 
     from .views import views
     app.register_blueprint(views, url_prefix="/")
